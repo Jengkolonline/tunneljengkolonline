70154f1c51ab36120899c42a5bfad547dec13913
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-06-22 15:09:13 +0700

    Add files via upload

---
+++ ssh/delete.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+               hariini=`date +%d-%m-%Y`
+               echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+               echo -e "\E[0;41;36m               AUTO DELETE                \E[0m"
+               echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
+               echo "Thank you for removing the EXPIRED USERS"
+               echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
+               cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+               totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+               for((i=1; i<=$totalaccounts; i++ ))
+               do
+               tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+               username=`echo $tuserval | cut -f1 -d:`
+               userexp=`echo $tuserval | cut -f2 -d:`
+               userexpireinseconds=$(( $userexp * 86400 ))
+               tglexp=`date -d @$userexpireinseconds`             
+               tgl=`echo $tglexp |awk -F" " '{print $3}'`
+               while [ ${#tgl} -lt 2 ]
+               do
+               tgl="0"$tgl
+               done
+               while [ ${#username} -lt 15 ]
+               do
+               username=$username" " 
+               done
+               bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+               echo "echo "Expired- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
+               todaystime=`date +%s`
+               if [ $userexpireinseconds -ge $todaystime ] ;
+               then
+		    	:
+               else
+               echo "echo "Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
+	           echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
+               userdel $username
+               fi
+               done
+               echo " "
+               echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
+               
+               read -n 1 -s -r -p "Press any key to back on menu"
+               menu
+        
