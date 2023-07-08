676f359438fd60c17bb718ef00ab2cbd3363fad2
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-08 22:16:51 +0700

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -52,8 +52,8 @@ OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{prin
 OhpDB=`cat /root/log-install.txt | grep -w "OHP DBear" | cut -d: -f2 | awk '{print $1}'`
 OhpOVPN=`cat /root/log-install.txt | grep -w "OHP OpenVPN" | cut -d: -f2 | awk '{print $1}'`
 
+read -p "  Expired (Minutes): " timer
 Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
-hari="1"
 Pass=1
 Quota=1
 echo Ping Host
@@ -62,7 +62,7 @@ sleep 0.5
 echo Setting Password: $Pass
 sleep 0.5
 clear
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+useradd -e `date -d "1 days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
