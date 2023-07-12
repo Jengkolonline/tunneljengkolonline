cc6834ed9be878449bd33f65784009f51c455ba5
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-12 13:35:54 +0700

    Update m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -17,70 +17,59 @@ ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
 grenbo="\e[92;1m"
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-# Getting
-export CHATID="2105857557"
-export KEY="5672989052:AAH_GGouVS3rAmZ71nmdmRcloWAIZIQhiPc"
+export CHATID="1830342336"
+export KEY="6320035110:AAE6otySdlNHPhKom2PNtp8yZn8X8rZS7LE"
 export TIME="10"
 export URL="https://api.telegram.org/bot$KEY/sendMessage"
 clear
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/XolvaID/ip/main/ip | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-echo -e "${Lyellow}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "${Lred}                PERMISSION DENIED ! ${NC}"
-echo -e "${Lyellow}     Your VPS ${NC}( ${green}$MYIP${NC} ) ${Lyellow}Has been Banned "
-echo -e "         Buy access permissions for scripts "
-echo -e "                 Contact Admin :"
-echo -e "             ${green}Telegram t.me/Andresakti "
-echo -e "             WhatsApp wa.me/081282840785"
-echo -e "${Lyellow}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    exit 0
-fi
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
 }
-IZIN=$(curl https://raw.githubusercontent.com/XolvaID/ip/main/ip | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m" >/dev/null 2>&1
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-echo -e "\e[32mloading...\e[0m"
+checking_sc
 clear
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[0;35m┌──────────────────────────────────────────┐\033[0m"
 echo -e "             MENU SHADOWSHOCKS            $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[0;35m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[0;35m┌──────────────────────────────────────────┐\033[0m"
 echo -e "  ${ORANGE}1.${NC} \033[0;36m Create Account Shadowshocks WS/GRPC${NC}"
 echo -e "  ${ORANGE}2.${NC} \033[0;36m Delete Account Shadowsocks WS/GRPC${NC}"
 echo -e "  ${ORANGE}3.${NC} \033[0;36m Renew Shadowsocks Account${NC}"
 echo -e "  ${ORANGE}4.${NC} \033[0;36m Check Shadowsocks login Account${NC}"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e ""
+echo -e "\033[0;35m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
 read -p "Select From Options [ 1 - 4 ] : " menu
 case $menu in
-1)
-    addss
-    ;;
-2)
-    delss
-    ;;
-3)
-    renewss
-    ;;
-4)
-    cekss
-    ;;
-*)
-    menu
-    ;;
+1) clear ; add-ssws ;;
+2) clear ; trialssws ;;
+3) clear ; renew-ssws ;;
+4) clear ; del-ssws ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo "salah tekan" ; sleep 1 ; m-ssws ;;
 esac
